3096cc7fb75e166ffa631299a47d9c7d401d6685
Jan Brümmer <jan.bruemmer12@gmail.com>
2019-11-27 13:50:39 +0100

    Implement createPerceptions method.

--- grid_sim/src/world/ObjectDetection.cpp
+++ grid_sim/src/world/ObjectDetection.cpp
@@ -22,15 +22,47 @@ ObjectDetection::ObjectDetection(srgsim::ServiceRobot* robot)
 
 std::vector<CellPerceptions> ObjectDetection::createPerceptions(World* world)
 {
-    //TODO implement me
+    std::map<Coordinate, Cell*> grid = world->getGrid();
     std::vector<CellPerceptions> cellPerceptionsList;
-    //TODO implement me
+
+    std::map<Coordinate, Cell*>::iterator it;
+    for ( it = grid.begin(); it != grid.end(); it++ )
+    {
+        CellPerceptions cellPerceptions;
+
+        std::cout << it->first  // string (key)
+                  << ':'
+                  << it->second   // string's value
+                  << std::endl ;
+
+        cellPerceptions.x = it->first.x;
+        cellPerceptions.y = it->first.y;
+        std::vector<Object*> objects = it->second->getObjects();
+
+        for(int i = 0; i < objects.size(); i++){
+            Perception* perception = new Perception();
+            Object* object = objects.at(i);
+            perception->state = object->getState();
+            perception->type = object->getType();
+            perception->objectID = object->getID();
+            perception->x = cellPerceptions.x;
+            perception->y = cellPerceptions.y;
+            cellPerceptions.perceptions.push_back(*perception);
+        }
+
+        cellPerceptionsList.push_back(cellPerceptions);
+    }
+
     return cellPerceptionsList;
 }
 
 std::vector<const Cell*> ObjectDetection::collectCells(Coordinate start, Coordinate end, World* world)
 {
     std::vector<const Cell*> cells;
+
+    std::map<Coordinate, Cell *> grid = world->getGrid();
+    world->getCell(start);
+
     //TODO implement me
     return cells;
 }
